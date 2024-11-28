fc513e773ad7f43d2b1cdf9f6bd5e126a8aeea56
20130290_NgoMinhKhanh <ngominhkhanh2002@gmail.com>
2024-11-28 22:45:28 +0700

    home view

---
+++ my_flutter_app/lib/pages/home.dart
@@ -0,0 +1,278 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:my_flutter_app/widget/widget_support.dart';
+
+class Home extends StatefulWidget {
+  const Home({super.key});
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  bool food = false, drink = false, fruits = false, ice_cream = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Container(
+      margin: EdgeInsets.only(top: 50, left: 20),
+      child: Column(
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text("Hello Minh Khánh,", style: AppWidget.boldTextFieldStyle()),
+              Container(
+                margin: EdgeInsets.only(right: 20),
+                padding: EdgeInsets.all(3),
+                decoration: BoxDecoration(
+                    color: Colors.black,
+                    borderRadius: BorderRadius.circular(8)),
+                child: Icon(
+                  Icons.shopping_cart,
+                  color: Colors.white,
+                ),
+              )
+            ],
+          ),
+          SizedBox(
+            height: 20,
+          ),
+          Text("Quán Ăn Sinh Diên", style: AppWidget.HeadLineTextFieldStyle()),
+          Text("Ngon, bổ và tiện lợi.", style: AppWidget.LightTextFieldStyle()),
+          SizedBox(
+            height: 20,
+          ),
+          Container(margin: EdgeInsets.only(right: 20), child: showItem()),
+          SizedBox(
+            height: 30,
+          ),
+          SingleChildScrollView(
+            scrollDirection: Axis.horizontal,
+            child: Row(
+              children: [
+                Container(
+                  margin: EdgeInsets.all(4),
+                  child: Material(
+                    elevation: 5,
+                    borderRadius: BorderRadius.circular(20),
+                    child: Container(
+                      padding: EdgeInsets.all(14),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Image.asset(
+                            "images/food/comtam.png",
+                            height: 150,
+                            width: 150,
+                            fit: BoxFit.cover,
+                          ),
+                          Text(
+                            "Cơm tấm",
+                            style: AppWidget.semiBoldTextFieldStyle(),
+                          ),
+                          SizedBox(
+                            height: 5,
+                          ),
+                          Text(
+                            "Ngon nhứt náck",
+                            style: AppWidget.LightTextFieldStyle(),
+                          ),
+                          SizedBox(
+                            height: 5,
+                          ),
+                          Text(
+                            "\30000vnđ",
+                            style: AppWidget.semiBoldTextFieldStyle(),
+                          )
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  width: 15,
+                ),
+                Container(
+                  margin: EdgeInsets.all(4),
+                  child: Material(
+                    elevation: 5,
+                    borderRadius: BorderRadius.circular(20),
+                    child: Container(
+                      padding: EdgeInsets.all(14),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Image.asset(
+                            "images/food/comtam.png",
+                            height: 150,
+                            width: 150,
+                            fit: BoxFit.fill,
+                          ),
+                          Text(
+                            "Cơm tấm",
+                            style: AppWidget.semiBoldTextFieldStyle(),
+                          ),
+                          SizedBox(
+                            height: 5,
+                          ),
+                          Text(
+                            "Ngon nhứt náck",
+                            style: AppWidget.LightTextFieldStyle(),
+                          ),
+                          SizedBox(
+                            height: 5,
+                          ),
+                          Text(
+                            "\30000vnđ",
+                            style: AppWidget.semiBoldTextFieldStyle(),
+                          )
+                        ],
+                      ),
+                    ),
+                  ),
+                )
+              ],
+            ),
+          ),
+          SizedBox(
+            height: 30,
+          ),
+          Container(
+            child: Row(
+              children: [
+                Image.asset(
+                  "images/food/bunpo.png",
+                  height: 120,
+                  width: 120,
+                  fit: BoxFit.fill,
+                ),
+                Column(
+                  children: [
+                    Text(
+                      "Bún bò Huế",
+                      style: AppWidget.boldTextFieldStyle(),
+                    )
+                  ],
+                )
+              ],
+            ),
+          ),
+        ],
+      ),
+    ));
+  }
+
+  Widget showItem() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        GestureDetector(
+          onTap: () {
+            food = true;
+            drink = false;
+            fruits = false;
+            ice_cream = false;
+
+            setState(() {});
+          },
+          child: Material(
+            elevation: 5,
+            borderRadius: BorderRadius.circular(10),
+            child: Container(
+                decoration: BoxDecoration(
+                    color: food ? Colors.black : Colors.white,
+                    borderRadius: BorderRadius.circular(10)),
+                padding: EdgeInsets.all(8),
+                child: Image.asset(
+                  "images/icons/food.png",
+                  height: 50,
+                  width: 50,
+                  fit: BoxFit.cover,
+                  color: food ? Colors.white : Colors.black,
+                )),
+          ),
+        ),
+        GestureDetector(
+          onTap: () {
+            food = false;
+            drink = true;
+            fruits = false;
+            ice_cream = false;
+
+            setState(() {});
+          },
+          child: Material(
+            elevation: 5,
+            borderRadius: BorderRadius.circular(10),
+            child: Container(
+                decoration: BoxDecoration(
+                    color: drink ? Colors.black : Colors.white,
+                    borderRadius: BorderRadius.circular(10)),
+                padding: EdgeInsets.all(8),
+                child: Image.asset(
+                  "images/icons/drink.png",
+                  height: 50,
+                  width: 50,
+                  fit: BoxFit.cover,
+                  color: drink ? Colors.white : Colors.black,
+                )),
+          ),
+        ),
+        GestureDetector(
+          onTap: () {
+            food = false;
+            drink = false;
+            fruits = true;
+            ice_cream = false;
+
+            setState(() {});
+          },
+          child: Material(
+            elevation: 5,
+            borderRadius: BorderRadius.circular(10),
+            child: Container(
+                decoration: BoxDecoration(
+                    color: fruits ? Colors.black : Colors.white,
+                    borderRadius: BorderRadius.circular(10)),
+                padding: EdgeInsets.all(8),
+                child: Image.asset(
+                  "images/icons/fruits.png",
+                  height: 50,
+                  width: 50,
+                  fit: BoxFit.cover,
+                  color: fruits ? Colors.white : Colors.black,
+                )),
+          ),
+        ),
+        GestureDetector(
+          onTap: () {
+            food = false;
+            drink = false;
+            fruits = false;
+            ice_cream = true;
+
+            setState(() {});
+          },
+          child: Material(
+            elevation: 5,
+            borderRadius: BorderRadius.circular(10),
+            child: Container(
+                decoration: BoxDecoration(
+                    color: ice_cream ? Colors.black : Colors.white,
+                    borderRadius: BorderRadius.circular(10)),
+                padding: EdgeInsets.all(8),
+                child: Image.asset(
+                  "images/icons/ice_cream.png",
+                  height: 50,
+                  width: 50,
+                  fit: BoxFit.cover,
+                  color: ice_cream ? Colors.white : Colors.black,
+                )),
+          ),
+        )
+      ],
+    );
+  }
+}
