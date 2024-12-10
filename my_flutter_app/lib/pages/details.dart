2d8a6b954aeb78952c6bc040130f3bef9bf79454
20130290_NgoMinhKhanh <ngominhkhanh2002@gmail.com>
2024-12-11 02:26:18 +0700

    detail page

--- my_flutter_app/lib/pages/details.dart
+++ my_flutter_app/lib/pages/details.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+import 'package:my_flutter_app/widget/widget_support.dart';
 
 class Details extends StatefulWidget {
   const Details({super.key});
@@ -8,6 +10,7 @@ class Details extends StatefulWidget {
 }
 
 class _DetailsState extends State<Details> {
+  int a = 1;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -18,6 +21,7 @@ class _DetailsState extends State<Details> {
           right: 20,
         ),
         child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             GestureDetector(
               onTap: () {
@@ -28,7 +32,165 @@ class _DetailsState extends State<Details> {
                 color: Colors.black,
               ),
             ),
-            Image.asset("images/food/bunpo.png")
+            Image.asset(
+              "images/food/bunpo.png",
+              width: MediaQuery.of(context).size.width,
+              height: MediaQuery.of(context).size.height / 3,
+              fit: BoxFit.fill,
+            ),
+            SizedBox(
+              height: 15,
+            ),
+            Row(
+              children: [
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      "Bún bò Huế",
+                      style: AppWidget.semiBoldTextFieldStyle(),
+                    ),
+                    Text(
+                      "Thơm ngon bổ dưỡng",
+                      style: AppWidget.boldTextFieldStyle(),
+                    ),
+                  ],
+                ),
+                Spacer(),
+                GestureDetector(
+                  onTap: () {
+                    if (a > 1) {
+                      --a;
+                    }
+                    --a;
+                    setState(() {});
+                  },
+                  child: Container(
+                    decoration: BoxDecoration(
+                        color: Colors.black,
+                        borderRadius: BorderRadius.circular(4)),
+                    child: Icon(
+                      Icons.remove,
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  width: 20,
+                ),
+                //số lượng
+                Text(
+                  a.toString(),
+                  style: AppWidget.semiBoldTextFieldStyle(),
+                ),
+                SizedBox(
+                  width: 20,
+                ),
+                GestureDetector(
+                  onTap: () {
+                    ++a;
+                    setState(() {});
+                  },
+                  child: Container(
+                    decoration: BoxDecoration(
+                        color: Colors.black,
+                        borderRadius: BorderRadius.circular(4)),
+                    child: Icon(
+                      Icons.add,
+                      color: Colors.white,
+                    ),
+                  ),
+                )
+              ],
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            //description
+            Text(
+              "Đây là món ăn đặc sản ở Huế được chọn lọc từ những nguyên liệu tốt nhất",
+              maxLines: 3,
+              style: AppWidget.LightTextFieldStyle(),
+            ),
+            SizedBox(
+              height: 30,
+            ),
+            Row(
+              children: [
+                Text(
+                  "Delivery Time",
+                  style: AppWidget.semiBoldTextFieldStyle(),
+                ),
+                Icon(
+                  Icons.alarm,
+                  color: Colors.black54,
+                ),
+                SizedBox(
+                  height: 5,
+                ),
+                Text(
+                  "30 phút",
+                  style: AppWidget.semiBoldTextFieldStyle(),
+                ),
+              ],
+            ),
+            Spacer(),
+            Padding(
+              padding: const EdgeInsets.only(bottom: 40),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        "Total Price",
+                        style: AppWidget.semiBoldTextFieldStyle(),
+                      ),
+                      Text(
+                        "\30.000",
+                        style: AppWidget.HeadLineTextFieldStyle(),
+                      )
+                    ],
+                  ),
+                  Container(
+                    width: MediaQuery.of(context).size.width / 2,
+                    padding: EdgeInsets.all(8),
+                    decoration: BoxDecoration(
+                        color: Colors.black,
+                        borderRadius: BorderRadius.circular(10)),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      children: [
+                        Text(
+                          "Add to cart",
+                          style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 16,
+                              fontFamily: 'Poppins'),
+                        ),
+                        SizedBox(
+                          width: 30,
+                        ),
+                        Container(
+                          padding: EdgeInsets.all(3),
+                          decoration: BoxDecoration(
+                              color: Colors.grey,
+                              borderRadius: BorderRadius.circular(8)),
+                          child: Icon(
+                            Icons.shopping_cart_outlined,
+                            color: Colors.white,
+                          ),
+                        ),
+                        SizedBox(
+                          width: 10,
+                        ),
+                      ],
+                    ),
+                  )
+                ],
+              ),
+            )
           ],
         ),
       ),
